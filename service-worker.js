811508ba955dcd5544e2cc9b2ee0ac15f7bd8837
patrick <patrickvdh92@gmail.com>
2025-02-01 16:16:09 +0100

    Update manifest.json voor offline gebruik

--- service-worker.js
+++ service-worker.js
@@ -7,21 +7,46 @@ const ASSETS_TO_CACHE = [
   '/pm1_vsp_lijst.json',
   '/pm2_vsp_lijst.json',
   '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/icons/icon-512x512.png',
+  '/manifest.json'
 ];
 
+// Installeer en cache alle bestanden
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
+// Nieuwe fetch strategie: Cache First, dan pas network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        // Return cached response direct als we die hebben
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        
+        // Anders proberen we het netwerk
+        return fetch(event.request)
+          .then((response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone de response omdat we hem twee keer willen gebruiken
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
   );
 });
